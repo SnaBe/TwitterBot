8f07c55061f8a5de1a628232d03ab80e13a57039
Simon <fynboen1999@gmail.com>
2017-04-10 15:43:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,20 @@
+console.log("The Bot Is Starting...");
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+console.log("Loaded The Bot Config");
+
+var params = {
+  q: 'Trump since:2016-07-11',
+  count: 100
+}
+
+T.get('search/tweets', params gotData);
+
+function gotData(err, data, response) {
+  console.log(data);
+})
 
